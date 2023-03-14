9432150998b864ba887a40c2f14349422e2adfab
Mod <87588981+Eto0x0@users.noreply.github.com>
2023-03-13 19:29:29 -0500

    Update test.c

--- test.c
+++ test.c
@@ -4,12 +4,12 @@ int main(int argc, char *argv[])
 {
 
     int numberOfArguments = argc;
-    char *argument1 = argv[0];
+    char *argument1 = argv[0];  #commands line agrument
     char *argumnet2 = argv[1];
     
     printf("Number of arguments: %d", numberOfArguments);
     printf("Argumnet 1 is the program name: %s",argument1);
-    printf("Argument 2 is the command linee argument %s",argumnet2);
+    printf("Argument 2 is the command line argument %s",argumnet2);
 
 return 0;
 
